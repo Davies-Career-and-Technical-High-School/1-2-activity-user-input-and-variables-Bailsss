f7399c53e967582c842639b20803d1c988250270
Bailey Henry <bailey.henry@daviestech.org>
2020-10-02 02:21:41 +0000

    done

--- Question2.java
+++ Question2.java
@@ -1,11 +1,24 @@
 /* Activity 1.2 - Question 2 */
-
+import java.util.Scanner;
 
 
 class Question2 {
     public static void main(String[] args) {
+      Scanner scan = new Scanner
+      (System.in);
+    String name1;
+    String name2;
+    String name3;
 
-        /* Write your code here */
+    System.out.println("give me threeee names one by one");
+
+    name1 = scan.nextLine();
+    name2 = scan.nextLine();
+    name3 = scan.nextLine();
+
+    System.out.print(name3 + " " + name2 + " " + name1);
+
+
 
     }
 }
--- Question3.java
+++ Question3.java
@@ -1,11 +1,21 @@
 /* Activity 1.2 - Question 3 */
-
+import java.util.Scanner;
 
 
 class Question3 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+    String name; 
+    String adj;
+
+    System.out.println("Hi there. What is your name?");
+
+    System.out.println("What adjective describes you?");
+
+    name = scan.nextLine();
+    adj = scan.nextLine();
+        System.out.println("My name is " + name + ". I am " + adj);      
 
     }
 }
